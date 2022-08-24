c91102292ad4d643265386a5cbf26e08f771d31f
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-08-24 23:22:56 +0300

    SnackBar was completed.

--- app/src/main/java/com/metehanbolat/snackbarcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/snackbarcompose/MainActivity.kt
@@ -3,15 +3,60 @@ package com.metehanbolat.snackbarcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.metehanbolat.snackbarcompose.ui.theme.SnackbarComposeTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SnackbarComposeTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DisplaySnackBar()
+                }
             }
         }
+    }
+}
+
+@Composable
+fun DisplaySnackBar() {
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val coroutineScope: CoroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier.padding(paddingValues).fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar(
+                            message = "This is the message",
+                            actionLabel = "Okay",
+                            duration = SnackbarDuration.Long
+                        )
+                    }
+                }
+            ) {
+                Text(text = "Display SnackBar")
+            }
+        }
+
     }
 }
